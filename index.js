afbc703487cfad05c1dd0e04887719c79fcc66f6
velocichaptor <jake.m.chappelle@gmail.com>
2021-02-02 08:37:11 -0600

    commit tree b9a64f89c5230dab5079935ad3fae98221653243

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
+function greet(timeString) {
+  const hour = parseInt(timeString, 10);
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of greet() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
 /* Write your implementation of displayMessage() */
